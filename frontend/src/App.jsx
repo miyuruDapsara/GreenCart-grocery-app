61be90d431897e7894e322fc86e913d74b26c9e2
miyuruDapsara <136076012+miyuruDapsara@users.noreply.github.com>
2025-09-05 15:10:16 +0530

    mainbanner.jsx file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,24 @@
-
-import Navbar from './components/navbar'
+import { Route, Routes, useLocation } from "react-router-dom";
+import Navbar from "./components/navbar";
+import Home from "./pages/Home";
 
 function App() {
 
+  const isSellerPath =useLocation().pathname.includes("seller");
+
+
   return (
-    <>
-     <Navbar/>
-    </>
-  )
+    <div>
+     {isSellerPath ? null : <Navbar />}
+      <div
+        className={`${isSellerPath ? "" : "px-6 md:px:16 lg:px-24 xl:px-32"} `}
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/Categories.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Categories = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Categories
+
+
+//1.10.23
